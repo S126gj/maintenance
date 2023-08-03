da81e3e78c3f234fabf1263e45eb0b3620a221a8
沈国纪 <1261550408@qq.com>
2023-08-03 16:19:29 +0800

    [fix] 优化代码

--- maintenance-mbg/src/main/java/com/device/mbg/auth/handler/StpInterfaceHandler.java
+++ maintenance-mbg/src/main/java/com/device/mbg/auth/handler/StpInterfaceHandler.java
@@ -1,8 +1,10 @@
 package com.device.mbg.auth.handler;
 
 import cn.dev33.satoken.stp.StpInterface;
+import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.json.JSONUtil;
 import com.device.common.utils.StringUtil;
+import com.device.mbg.auth.util.StpCustomerUtil;
 import com.device.mbg.domain.entity.Menu;
 import com.device.mbg.domain.entity.Role;
 import com.device.mbg.service.IRoleMenuService;
@@ -24,23 +26,18 @@ import java.util.stream.Collectors;
 @Component
 public class StpInterfaceHandler implements StpInterface {
 
-    // 用户登录 loginType --- 此值取决于 StpUtil 中的 Type 字段
-    private static final String USER_LOGIN_TYPE = "login";
-    // 客户登录 loginType --- 此值取决于 StpCustomerUtil 中的 Type 字段
-    private static final String CUSTOMER_LOGIN_TYPE = "customer";
-
     @Autowired
     private IRoleMenuService roleMenuService;
 
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
         List<String> permissionList = new ArrayList<>();
-        if (USER_LOGIN_TYPE.equals(loginType)) {
+        if (StpUtil.getLoginType().equals(loginType)) {
             List<Menu> menuList = roleMenuService.getMenuListByUserId(loginId.toString());
             // 过滤出不为空的权限
             permissionList = menuList.stream().map(Menu::getPermission).filter(StringUtil::isNotBlank).collect(Collectors.toList());
             log.info("[StpInterfaceImpl][getPermissionList] 用户 permissionList:{}", JSONUtil.toJsonStr(permissionList));
-        } else if (CUSTOMER_LOGIN_TYPE.equals(loginType)) {
+        } else if (StpCustomerUtil.getLoginType().equals(loginType)) {
 
             log.info("[StpInterfaceImpl][getPermissionList] 客户 permissionList:{}", JSONUtil.toJsonStr(permissionList));
         }
@@ -50,11 +47,11 @@ public class StpInterfaceHandler implements StpInterface {
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
         List<String> roleNameList = new ArrayList<>();
-        if (USER_LOGIN_TYPE.equals(loginType)) {
+        if (StpUtil.getLoginType().equals(loginType)) {
             List<Role> roleList = roleMenuService.getRoleListByUserId(loginId.toString());
             roleNameList = roleList.stream().map(Role::getName).collect(Collectors.toList());
             log.info("[StpInterfaceImpl][getRoleList] 用户 roleNameList:{}", JSONUtil.toJsonStr(roleNameList));
-        } else if (CUSTOMER_LOGIN_TYPE.equals(loginType)) {
+        } else if (StpCustomerUtil.getLoginType().equals(loginType)) {
 
             log.info("[StpInterfaceImpl][getRoleList] 客户 roleNameList:{}", JSONUtil.toJsonStr(roleNameList));
         }
