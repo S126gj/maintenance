aa60acc03bfeff8caae40d9107beea7e9344669a
沈国纪 <1261550408@qq.com>
2023-08-03 14:05:29 +0800

    [fix] 修复 shardingjdbc bug

--- maintenance-system/src/test/java/com/device/system/ShardTest.java
+++ maintenance-system/src/test/java/com/device/system/ShardTest.java
@@ -1,8 +1,14 @@
 package com.device.system;
 
+import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.device.system.core.entity.BoxRun;
+import com.device.system.core.entity.Dict;
+import com.device.system.core.entity.Machine;
 import com.device.system.core.mapper.BoxRunMapper;
+import com.device.system.core.mapper.DictMapper;
+import com.device.system.core.mapper.MachineMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +29,14 @@ class ShardTest {
 
     @Autowired
     private BoxRunMapper boxRunMapper;
+    @Autowired
+    private DictMapper dictMapper;
+    @Autowired
+    private MachineMapper machineMapper;
 
     @Test
     void insert() {
+        // 此表走分表规则
         for (int i = 0; i < 1000; i++) {
             BoxRun boxRun = BoxRun.builder()
                 .boxCode("123")
@@ -43,8 +54,25 @@ class ShardTest {
 
     @Test
     void query() {
+        // 此表走分表规则
         List<BoxRun> boxRuns = boxRunMapper.selectList(null);
         log.info("boxRuns:{}", JSONUtil.toJsonStr(boxRuns));
+    }
+
+    @Test
+    void dictQuery() {
+        // 此表不走分表规则
+        StpUtil.login("1");
+        List<Dict> dicts = dictMapper.selectList(null);
+        log.info("dicts:{}", JSONUtil.toJsonStr(dicts));
+    }
+
+    @Test
+    void machineQuery() {
+        // 此表不走分表规则
+        StpUtil.login("1");
+        List<Machine> machines = machineMapper.selectList(null);
+        log.info("machines:{}", JSONUtil.toJsonStr(machines));
     }
 
 }
