8095ab95a718c6b1f570dc533978781de4c420cb
沈国纪 <1261550408@qq.com>
2023-08-10 08:51:16 +0800

    [fix] 补全LocalDateTimeConfig不完整配置

--- maintenance-common/src/main/java/com/device/common/config/LocalDateTimeSerializerConfig.java
+++ maintenance-common/src/main/java/com/device/common/config/LocalDateTimeSerializerConfig.java
@@ -1,17 +1,23 @@
 package com.device.common.config;
 
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * 全局处理 LocalDate 格式
+ * 全局处理 LocalDate、LocalDateTime、LocalTime 格式
  * 如：2023-06-28T09:56:44 转换为 2023-06-28 09:56:44
  * @Author: Guoji Shen
  * @Date: 2023/6/30 13:30
@@ -19,34 +25,48 @@ import java.time.format.DateTimeFormatter;
 @Configuration
 public class LocalDateTimeSerializerConfig {
 
-    @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
-    private String pattern;
+    private static final String LOCAL_DATE_PATTERN = "yyyy-MM-dd";
+    private static final String LOCAL_TIME_PATTERN = "HH:mm:ss";
+    private static final String LOCAL_DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
-    /*
-     * 序列化内容
-     *   LocalDateTime -> String
-     * 服务端返回给客户端内容
-     * */
+
     @Bean
-    public LocalDateTimeSerializer localDateTimeSerializer() {
-        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
+    public LocalDateSerializer localDateSerializer() {
+        return new LocalDateSerializer(DateTimeFormatter.ofPattern(LOCAL_DATE_PATTERN));
     }
 
-    /*
-     * 反序列化内容
-     *   String -> LocalDateTime
-     * 客户端传入服务端数据
-     * */
+    @Bean
+    public LocalDateDeserializer localDateDeserializer() {
+        return new LocalDateDeserializer(DateTimeFormatter.ofPattern(LOCAL_DATE_PATTERN));
+    }
+
+    @Bean
+    public LocalTimeSerializer localTimeSerializer() {
+        return new LocalTimeSerializer(DateTimeFormatter.ofPattern(LOCAL_TIME_PATTERN));
+    }
+
+    @Bean
+    public LocalTimeDeserializer localTimeDeserializer() {
+        return new LocalTimeDeserializer(DateTimeFormatter.ofPattern(LOCAL_TIME_PATTERN));
+    }
+
+    @Bean
+    public LocalDateTimeSerializer localDateTimeSerializer() {
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(LOCAL_DATETIME_PATTERN));
+    }
+
     @Bean
     public LocalDateTimeDeserializer localDateTimeDeserializer() {
-        return new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(pattern));
+        return new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(LOCAL_DATETIME_PATTERN));
     }
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-        return builder -> {
-            builder.serializerByType(LocalDateTime.class, localDateTimeSerializer());
-            builder.deserializerByType(LocalDateTime.class, localDateTimeDeserializer());
-        };
+        return builder -> builder.serializerByType(LocalDate.class, localDateSerializer())
+            .serializerByType(LocalTime.class, localTimeSerializer())
+            .serializerByType(LocalDateTime.class, localDateTimeSerializer())
+            .deserializerByType(LocalDate.class, localDateDeserializer())
+            .deserializerByType(LocalTime.class, localTimeDeserializer())
+            .deserializerByType(LocalDateTime.class, localDateTimeDeserializer());
     }
 }
